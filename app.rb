a6eb04dd00a0aba69ded20909951125231f068d1
Elijah wines <elijahwines84@yahoo.com>
2020-05-05 17:47:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,39 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do 
+    @name = params[:name]
+    @reverse_name = @name.reverse!
+  end
+
+  get "/square/:number" do 
+    @number = params[:numeber].to_i
+    (@number*@number).to_s
+  end
+
+  get "/say/:number/:phrase" do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @return_array = []
+
+    @number.times do 
+      @return_array << @phrase
+    end
+
+    @return_array.join(' ')
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do 
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    @word1+" "+@word2+" "+@word3+" "+@word4+" "+@word5+"."
+  end
+
+  get "/operation/:number1/:number2" do 
+    
+  end
 
 end
